b9d28ab39105c5e2c24804d6fcce324e6694a1da
Cristiano De Pasquale <cristiano.depasquale@gmail.com>
2026-05-05 00:33:42 +0200

    check css pillole

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //  per forzare l'aggiornamento della cache su tutti i client.
 // ════════════════════════════════════════════════════════════════
 
-const CACHE_VERSION = 'fd-v1.1';
+const CACHE_VERSION = 'fd-v1.2';
 
 // Risorse da precachare all'installazione (shell dell'app)
 const PRECACHE = [
