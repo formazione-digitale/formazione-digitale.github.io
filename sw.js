53bbfb67740ff1f144f0d76f617bbe67cdf85f92
Cristiano De Pasquale <cristiano.depasquale@gmail.com>
2026-05-04 23:56:06 +0200

    refresh cache

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //  per forzare l'aggiornamento della cache su tutti i client.
 // ════════════════════════════════════════════════════════════════
 
-const CACHE_VERSION = 'fd-v1';
+const CACHE_VERSION = 'fd-v1.1';
 
 // Risorse da precachare all'installazione (shell dell'app)
 const PRECACHE = [
